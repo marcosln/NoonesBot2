a18943d1fb19fafd2037f8e7df73e790bec4e4c4
marcosln <78895559+marcosln@users.noreply.github.com>
2025-02-20 18:56:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,25 +5,25 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
-    # Si es una solicitud GET, verificar si contiene el desafío
     if request.method == 'GET':
         challenge = request.headers.get('X-Noones-Request-Challenge')
         if challenge:
             return Response(challenge, mimetype='text/plain')
         else:
-            # Si no hay desafío, devolver un mensaje simple
             return Response("Webhook endpoint activo", status=200)
     
-    # Si es una solicitud POST, procesar como notificación
     if request.method == 'POST':
         # Verificar que el tipo de contenido sea 'application/json'
+        print("Encabezados:", request.headers)
+        print("Cuerpo de la solicitud:", request.get_data())
+        
         if request.content_type != 'application/json':
             print(f"Tipo de contenido incorrecto: {request.content_type}")
             return Response('Unsupported Media Type', status=415)
+
         try:
             data = request.json
             print("Notificación recibida:", data)
-            # Aquí puedes agregar la lógica para procesar la notificación
             return Response("OK", status=200)
         except Exception as e:
             print("Error al procesar JSON:", e)
